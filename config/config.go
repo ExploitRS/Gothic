8810f86084ac5ff66e3812b4d09b8e390e96667c
Nyansomware Gang <nillability@protonmail.com>
2023-03-23 00:21:29 +0900

    Update config.go

--- config/config.go
+++ config/config.go
@@ -1,7 +1,12 @@
 package config
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/exploit-rs/gothic/config/database"
+
+	toml "github.com/pelletier/go-toml/v2"
 )
 
 type Mode string
@@ -16,6 +21,7 @@ type Config struct {
 	server   *Server
 	database *database.Database
 	mode     Mode
+	version  uint
 }
 
 func (c *Config) New() *Config {
@@ -25,4 +31,10 @@ func (c *Config) New() *Config {
 	}
 }
 
-func (c *Config) Load(path string) {}
+func (c *Config) Load(path string) {
+	read, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Println("Error ", err.Error())
+	}
+	toml.Unmarshal(read, &c)
+}
